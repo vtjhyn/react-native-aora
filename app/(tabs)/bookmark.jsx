24d0d2c38ed8f1242e8a751c921ef1d6f3bad222
vtjhyn <vtjahyono10@gmail.com>
2024-05-16 22:13:40 +0700

    create: bookmark tabs

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,61 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, RefreshControl, Image } from "react-native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import SearchInput from "../../components/SearchInput";
+import { getSavedPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { images } from "../../constants";
 
 const Bookmark = () => {
-  return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
-  )
-}
+  const { user } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(() => getSavedPosts(user.$id));
+  const [refreshing, setRefreshing] = useState(false);
 
-export default Bookmark
+  const onRefresh = async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="text-2xl font-psemibold text-white">
+              Saved Videos
+            </Text>
+            <View className="mt-6 mb-8">
+              <SearchInput />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <View className="justify-center items-center px-4">
+            <Image
+              source={images.empty}
+              className="w-[270px] h-[215px]"
+              resizeMode="contain"
+            />
+            <Text className="text-xl text-center font-psemibold text-white mt-2">
+              No Videos Found
+            </Text>
+            <Text className="font-pmedium text-sm text-gray-100">
+              There's no videos added to bookmark
+            </Text>
+          </View>
+        )}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Bookmark;
